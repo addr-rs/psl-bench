7466b8806c24fe16c6bed6b1a453796b4401202c
rushmorem <rushmore@webenchanter.com>
2018-07-27 22:15:24 +0200

    add more crates

--- cargo/benches/psl_benchmark.rs
+++ cargo/benches/psl_benchmark.rs
@@ -1,10 +1,10 @@
 extern crate psl;
+extern crate publicsuffix;
+extern crate addr;
 #[macro_use]
 extern crate criterion;
 
 use std::process::Command;
-
-use psl::{Psl, List};
 use criterion::Criterion;
 
 fn pypy_cmd() -> Command {
@@ -15,11 +15,21 @@ fn pypy_cmd() -> Command {
 
 fn bench(c: &mut Criterion) {
     let domains = vec!["example.gb.com"];
+    let list = publicsuffix::List::fetch().unwrap();
 
-    let list = List::new();
+    c.bench_function_over_inputs("Rust - addr", move |b, &domain| {
+        use std::str::FromStr;
+        use addr::DomainName;
+        b.iter(|| { DomainName::from_str(domain).unwrap(); } )
+    }, domains.clone());
 
-    c.bench_function_over_inputs("Rust", move |b, &domain| {
-        b.iter(|| { list.domain(domain).unwrap(); } )
+    c.bench_function_over_inputs("Rust - publicsuffix", move |b, &domain| {
+        b.iter(|| { list.parse_domain(domain).unwrap(); } )
+    }, domains.clone());
+
+    c.bench_function_over_inputs("Rust - psl", move |b, &domain| {
+        use psl::{Psl, List};
+        b.iter(|| { List.domain(domain).unwrap(); } )
     }, domains.clone());
 
     c.bench_program_over_inputs("PyPy", pypy_cmd, domains);
